d8c87e0ebf37501de3b5842384e34041938e1ca6
valda-z <valdemar.zavadsky@microsoft.com>
2017-05-15 08:37:53 +0200

    Add project files.

---
+++ windockertest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace windockertest
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    // Used to inject in the MVC framework
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+
+    // Example controller
+    public class TestController : Controller
+    {
+        [HttpGet("/")]
+        public object Index()
+        {
+            return "Docker is running there => COMPUTERNAME: " + Environment.GetEnvironmentVariable("COMPUTERNAME");
+        }
+    }
+}
